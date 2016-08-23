6c1916a64b5e1ba7379fe0c699e57950b52bd6c8
Z.d.Peacock <zdp@thoomtech.com>
2016-08-23 15:41:05 -0600

    Updated version.

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,6 +1,6 @@
 module Thoom
   class Constants
-    VERSION   = '0.14.2'.freeze
+    VERSION   = '0.14.3'.freeze
     MIME_JSON = 'application/json'.freeze
     MIME_XML  = 'application/xml'.freeze
   end
